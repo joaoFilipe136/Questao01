b7f243caef42fb2410702b68b26d046170bf3e5e
Joao Filipe <florentinojoaofilipe644@gmail.com>
2021-11-06 12:19:55 -0300

    Envio a questão 01

---
+++ ident_id.php
@@ -0,0 +1,9 @@
+<?php
+$Nome = $_GET['nome'];
+$Idade = $_GET['idade'];
+$Altura = $_GET['altura'];
+
+ echo "NOME: ".$Nome."<br>";
+ echo "IDADE: ".$Idade."<br>";
+ echo "ALTURA: ".$Altura."<br>";
+ ?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title> Questão 1 </title>
+</head>
+<body>
+   
+   <p><a href="ident_id.php?nome=maria&idade=18&altura=1.7"> Passar valores </a></p>
+
+</body>
+</html>
